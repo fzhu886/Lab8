b6bb1fbc1e9029a765b197a6a02adbe73d7ef755
Fan Zhu <fzhu@ualberta.ca>
2022-10-25 17:24:55 -0600

    Testing GitHub actions

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -45,4 +45,13 @@ public class CustomList extends ArrayAdapter<City> {
 
     }
 
+    public int getCount() {
+
+        return cities.size();
+    }
+
+    public void addCity(City city) {
+
+    }
+
 }
